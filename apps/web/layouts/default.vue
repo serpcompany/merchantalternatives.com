8f952baca6d474ea481679402834deffca76f45c
Niall Quirke <quirkeniall@gmail.com>
2024-06-06 10:25:26 +0100

    added password proctection

--- apps/web/layouts/default.vue
+++ apps/web/layouts/default.vue
@@ -1,7 +1,33 @@
+<script setup lang="ts">
+  const authorised = ref(false);
+  const password = ref("");
+
+  const checkPassword = () => {
+    if (password.value === "password") {
+      authorised.value = true;
+    }
+  };
+</script>
+
 <template>
-  <NavBar />
-  <main class="min-h-screen pt-20">
-    <slot />
-  </main>
-  <Footer />
+  <div>
+    <div v-if="!authorised" class="flex h-screen items-center justify-center">
+      <form @submit.prevent="checkPassword">
+        <Input
+          type="password"
+          v-model="password"
+          placeholder="Enter password"
+          class="w-full"
+        />
+        <Button type="submit" class="mt-4 w-full"> Submit </Button>
+      </form>
+    </div>
+    <div v-else>
+      <NavBar />
+      <main class="min-h-screen pt-20">
+        <slot />
+      </main>
+      <Footer />
+    </div>
+  </div>
 </template>
